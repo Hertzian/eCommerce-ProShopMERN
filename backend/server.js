1d30b29f1b60b39052b8209ae5a24769fb25c868
Lalitus <tallitosan@gmail.com>
2020-10-17 15:01:05 -0500

    fetching products from react (useEffect)

--- backend/server.js
+++ backend/server.js
@@ -16,4 +16,4 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 500'))
+app.listen(5000, console.log('Server running on port 5000'))
