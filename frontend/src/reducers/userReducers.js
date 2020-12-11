8c815de0ebd37b9ce03bdda26035f8fefd51b0ff
Lalitus <tallitosan@gmail.com>
2020-12-10 22:35:05 -0600

    user register reducer, action & screen

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,9 +1,14 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/userConstants"
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+} from '../constants/userConstants'
 
-export const userLoginReducer = (
-  state = {  },
-  action
-) => {
+export const userLoginReducer = (state = {}, action) => {
   //params are: initial state & action to dispatch
   switch (action.type) {
     case USER_LOGIN_REQUEST:
@@ -13,7 +18,21 @@ export const userLoginReducer = (
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload }
     case USER_LOGOUT:
-      return {  }
+      return {}
+    default:
+      return state
+  }
+}
+
+export const userRegisterReducer = (state = {}, action) => {
+  //params are: initial state & action to dispatch
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true }
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload }
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload }
     default:
       return state
   }
