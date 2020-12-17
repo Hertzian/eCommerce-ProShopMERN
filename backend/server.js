889f331a33e1c3baca06ebd3d0c4184764d1c063
Lalitus <tallitosan@gmail.com>
2020-12-17 01:48:32 -0600

    adding paypal payments

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express  from 'express'
-import dotenv  from 'dotenv'
+import express from 'express'
+import dotenv from 'dotenv'
 import colors from 'colors'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -24,9 +24,18 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+)
