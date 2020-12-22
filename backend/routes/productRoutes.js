2505ca4b4d2ebea13feb7d3ca45fb9605ed040c4
Lalitus <tallitosan@gmail.com>
2020-12-22 13:59:22 -0600

    morgan & crete review endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,11 +5,17 @@ import {
   getProducts,
   deleteProduct,
   createdProduct,
-  updateProduct
+  updateProduct,
+  createProductReview,
 } from '../controllers/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createdProduct)
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 
 export default router
