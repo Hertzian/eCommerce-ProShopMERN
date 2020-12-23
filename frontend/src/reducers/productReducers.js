668ce30631b78e5d2c1aa89c063410306e3692fd
Lalitus <tallitosan@gmail.com>
2020-12-22 18:59:08 -0600

    product pagination

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -22,82 +22,96 @@ import {
   PRODUCT_CREATE_REVIEW_RESET,
 } from '../constants/productConstants'
 
-export const productListReducer = (state = {products: []}, action) => { //params are: initial state & action to dispatch
+export const productListReducer = (state = { products: [] }, action) => {
+  //params are: initial state & action to dispatch
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-      return {loading: true, products: []}
+      return { loading: true, products: [] }
     case PRODUCT_LIST_SUCCESS:
-      return {loading: false, products: action.payload}
+      return {
+        loading: false,
+        products: action.payload.products,
+        pages: action.payload.pages,
+        page: action.payload.page,
+      }
     case PRODUCT_LIST_FAIL:
-      return {loading: false, error: action.payload}
+      return { loading: false, error: action.payload }
     default:
       return state
   }
 }
 
-export const productDetailsReducer = (state = {product: {reviews: []}}, action) => { //params are: initial state & action to dispatch
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  //params are: initial state & action to dispatch
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
-      return {loading: true, ...state}
+      return { loading: true, ...state }
     case PRODUCT_DETAILS_SUCCESS:
-      return {loading: false, product: action.payload}
+      return { loading: false, product: action.payload }
     case PRODUCT_DETAILS_FAIL:
-      return {loading: false, error: action.payload}
+      return { loading: false, error: action.payload }
     default:
       return state
   }
 }
 
-export const productDeleteReducer = (state = {}, action) => { //params are: initial state & action to dispatch
+export const productDeleteReducer = (state = {}, action) => {
+  //params are: initial state & action to dispatch
   switch (action.type) {
     case PRODUCT_DELETE_REQUEST:
-      return {loading: true}
+      return { loading: true }
     case PRODUCT_DELETE_SUCCESS:
-      return {loading: false, success: true}
+      return { loading: false, success: true }
     case PRODUCT_DELETE_FAIL:
-      return {loading: false, error: action.payload}
+      return { loading: false, error: action.payload }
     default:
       return state
   }
 }
 
-export const productCreateReducer = (state = {}, action) => { //params are: initial state & action to dispatch
+export const productCreateReducer = (state = {}, action) => {
+  //params are: initial state & action to dispatch
   switch (action.type) {
     case PRODUCT_CREATE_REQUEST:
-      return {loading: true}
+      return { loading: true }
     case PRODUCT_CREATE_SUCCESS:
-      return {loading: false, success: true, product: action.payload}
+      return { loading: false, success: true, product: action.payload }
     case PRODUCT_CREATE_FAIL:
-      return {loading: false, error: action.payload}
+      return { loading: false, error: action.payload }
     case PRODUCT_CREATE_RESET:
       return {}
     default:
       return state
   }
 }
-export const productUpdateReducer = (state = {product: {}}, action) => { //params are: initial state & action to dispatch
+export const productUpdateReducer = (state = { product: {} }, action) => {
+  //params are: initial state & action to dispatch
   switch (action.type) {
     case PRODUCT_UPDATE_REQUEST:
-      return {loading: true}
+      return { loading: true }
     case PRODUCT_UPDATE_SUCCESS:
-      return {loading: false, success: true, product: action.payload}
+      return { loading: false, success: true, product: action.payload }
     case PRODUCT_UPDATE_FAIL:
-      return {loading: false, error: action.payload}
+      return { loading: false, error: action.payload }
     case PRODUCT_UPDATE_RESET:
-      return {product: {}}
+      return { product: {} }
     default:
       return state
   }
 }
 
-export const productReviewCreateReducer = (state = {}, action) => { //params are: initial state & action to dispatch
+export const productReviewCreateReducer = (state = {}, action) => {
+  //params are: initial state & action to dispatch
   switch (action.type) {
     case PRODUCT_CREATE_REVIEW_REQUEST:
-      return {loading: true}
+      return { loading: true }
     case PRODUCT_CREATE_REVIEW_SUCCESS:
-      return {loading: false, success: true}
+      return { loading: false, success: true }
     case PRODUCT_CREATE_REVIEW_FAIL:
-      return {loading: false, error: action.payload}
+      return { loading: false, error: action.payload }
     case PRODUCT_CREATE_REVIEW_RESET:
       return {}
     default:
