a27d0d36232031d1f4782d9af647c17bf8848131
Lalo Aguilar <tallitosan@gmail.com>
2021-02-21 02:29:19 -0600

    added year into Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,12 +3,13 @@ import {Container, Row, Col} from 'react-bootstrap'
 
 
 const Footer = () => {
+  const date = new Date().getFullYear()
   return (
     <footer>
       <Container>
         <Row>
           <Col className="text-center py-3">
-            Copyright &copy Proshop
+            Copyright &copy; {date} Lalo Aguilar
           </Col>
         </Row>
       </Container>
